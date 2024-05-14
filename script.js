71a2f4ae2b5446c37b690c7734685a5ef1d5c228
Rajnikant <rajnikantb2002@gmail.com>
2024-05-14 19:10:03 +0530

    add increment in number with text

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const incrementBtn = document.querySelector('#increment')
 const decrementBtn = document.querySelector('#decrement')
+const ulelement = document.getElementById('items-list')
+
 let  counter=0
 function incrementCounter(){
    const incrementEl=document.getElementById('Counter')
    counter++
    incrementEl.innerText=counter
+   const li=document.createElement('li')
+   const textnode = document.createTextNode('Rajnikant'+ counter)
+   li.appendChild(textnode)
+   ulelement.appendChild(li)
 }
 function decrementCounter(){
    const decrementEl=document.getElementById('Counter')
